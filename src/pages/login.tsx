d9eee2c7e20bffe1080f5cbdff9b8f6687d8e619
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-20 18:57:26 -0500

    change logo to Yariga for login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -43,10 +43,7 @@ export const Login: React.FC = () => {
   return (
     <Box
       component="div"
-      sx={{
-        background: `radial-gradient(50% 50% at 50% 50%, #63386A 0%, #310438 100%)`,
-        backgroundSize: "cover",
-      }}
+      sx={{ backgroundColor: "#FCFCFC"}}
     >
       <Container
         component="main"
@@ -67,7 +64,7 @@ export const Login: React.FC = () => {
           }}
         >
           <div>
-            <img src="./refine.svg" alt="Refine Logo" />
+            <img src={yariga} alt="Yariga Logo" />
           </div>
           <Box mt={4}>
             <GoogleButton />
