e7bab1d09b17a537dcf31608d95725ebffa250d6
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-21 00:03:48 -0500

    add imports to homepage

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import { useList } from '@pankod/refine-core';
 
+import {
+  PieChart,
+  PropertyReferrals,
+  TotalRevenue,
+  PropertyCard,
+  TopAgent
+} from 'components';
+
 
 const home = () => {
   return (
