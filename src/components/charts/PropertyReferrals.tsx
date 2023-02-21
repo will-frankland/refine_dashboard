95515c831e9b6da1f877133fb55aaf1b662cb72b
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-21 00:59:16 -0500

    add property referrals stats with colors

--- src/components/charts/PropertyReferrals.tsx
+++ src/components/charts/PropertyReferrals.tsx
@@ -15,9 +15,19 @@ const ProgressBar = ({ title, percentage, color }: ProgressBarProps) => (
         {title}
       </Typography>
       <Typography fontSize={16} fontWeight={500} color="#11142d">
-        {percentage}
+        {percentage}%
       </Typography>
     </Stack>
+    <Box mt={2} position="relative" width="100%" height="8px" borderRadius={1} bgcolor="#e4e8ef">
+      <Box 
+        width={`${percentage}%`}
+        bgcolor={color}
+        position="absolute"
+        height="100%"
+        borderRadius={1}
+      />
+
+    </Box>
   </Box>
 )
 
