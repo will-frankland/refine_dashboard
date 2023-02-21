5ae9287daa1832eea0f18c6e80aff40eec02b36e
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-21 00:18:08 -0500

    add Typography components for title and value

--- src/components/charts/PieChart.tsx
+++ src/components/charts/PieChart.tsx
@@ -5,7 +5,10 @@ import { PieChartProps } from 'interfaces/home';
 const PieChart = ({ title, value, series, colors } : PieChartProps) => {
   return (
     <Box>
-
+      <Stack direction="column">
+        <Typography fontSize={14} color="#808191">{title}</Typography>
+        <Typography fontSize={24} color="#11142d" fontWeight={700} mt={1}>{value}</Typography>
+      </Stack>
     </Box>
   )
 }
