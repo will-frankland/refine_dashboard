60a6bcd1c824be994b62d886f8d81a6545d62b8c
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-21 00:36:53 -0500

    add styling to box component in TotalRev

--- src/components/charts/TotalRevenue.tsx
+++ src/components/charts/TotalRevenue.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import ReactApexChart from 'react-apexcharts';
+import { Box, Typography, Stack } from '@pankod/refine-mui';
+import { ArrowCircleUpRounded } from '@mui/icons-material';
+import { TotalRevenueOptions, TotalRevenueSeries } from './chart.config';
 
 const TotalRevenue = () => {
   return (
-    <div>
-      Total Revenue
-    </div>
+    <Box
+      p={4}
+      flex={1}
+      bgcolor="#FCFCFC"
+      id="chart"
+      display="flex"
+      flexDirection="column"
+      borderRadius="15px"
+      >
+
+    </Box>
   )
 }
 
