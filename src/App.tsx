4ede9a08f3a32131d402710a7ea07416fadc21c2
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-20 19:44:33 -0500

    add import of pages to App.tsx - error will occur, pages not made yet

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,16 @@ import routerProvider from "@pankod/refine-react-router-v6";
 import axios, { AxiosRequestConfig } from "axios";
 import { ColorModeContextProvider } from "contexts";
 import { Title, Sider, Layout, Header } from "components/layout";
-import { Login } from "pages/login";
+import { 
+  Login,
+  Home,
+  Agents,
+  MyProfile,
+  PropertyDetails,
+  AllProperties,
+  CreateProperty,
+  AgentProfiles,
+  EditProperty, } from "pages/login";
 import { CredentialResponse } from "interfaces/google";
 import { parseJwt } from "utils/parse-jwt";
 
@@ -140,6 +149,7 @@ function App() {
           routerProvider={routerProvider}
           authProvider={authProvider}
           LoginPage={Login}
+          DashboardPage={Home}
         />
       </RefineSnackbarProvider>
     </ColorModeContextProvider>
