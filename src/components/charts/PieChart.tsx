cb8e5adf2abbe7e140ecd46376edb8ea118f9733
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-21 00:21:43 -0500

    add ApexCharts and style to show on homepage

--- src/components/charts/PieChart.tsx
+++ src/components/charts/PieChart.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactApexChart from 'react-apexcharts';
 import { Box, Typography, Stack } from '@pankod/refine-mui';
 import { PieChartProps } from 'interfaces/home';
 
@@ -9,6 +10,17 @@ const PieChart = ({ title, value, series, colors } : PieChartProps) => {
         <Typography fontSize={14} color="#808191">{title}</Typography>
         <Typography fontSize={24} color="#11142d" fontWeight={700} mt={1}>{value}</Typography>
       </Stack>
+      <ReactApexChart
+        options={{
+          chart: { type: 'donut'},
+          colors,
+          legend: { show: false },
+          dataLabels: { enabled: false }
+        }}
+        series={series}
+        type="donut"
+        width="120px"
+      />
     </Box>
   )
 }
