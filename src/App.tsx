5f3c7d6e36ba6b6048154a0c8a5402e0b94031c7
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-22 12:35:22 -0500

    update agent, review, message and my-profile sections to utilize lists

--- src/App.tsx
+++ src/App.tsx
@@ -125,23 +125,24 @@ function App() {
             },
             {
               name: "agent",
-              list: MuiInferencer,
+              list: Agents,
+              show: AgentProfile,
               icon: <PeopleAltOutlined />
             },
             {
               name: "review",
-              list: MuiInferencer,
+              list: Home,
               icon : <StarOutlineRounded />
             },
             {
               name: "message",
-              list: MuiInferencer,
+              list: Home,
               icon: <ChatBubbleOutlined />
             },
             {
               name: "my-profile",
               options: { label: 'My Profile'},
-              list: MuiInferencer,
+              list: MyProfile,
               icon : <AccountCircleOutlined />
             },
           ]}
