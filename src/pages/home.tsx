cd10461f2a5b14524bffd623dbc31e0ddf4ac9a3
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-22 12:28:52 -0500

    add gap to stack between revenue and referrals to space out

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -43,7 +43,7 @@ const home = () => {
           colors={['#475be8', '#e4e8ef']}
           />
       </Box>
-      <Stack mt="25px" width="100%" direction={{ xs: 'column', lg: 'row' }}>
+      <Stack mt="25px" width="100%" direction={{ xs: 'column', lg: 'row' }} gap={4}>
         <TotalRevenue />
         <PropertyReferrals />
       </Stack>
