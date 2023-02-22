33d4095458cdb6692e15cb3d602d9439a3cbddcb
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-22 12:41:55 -0500

    add Box and Stack to allProperties and add customButton with props

--- src/pages/all-properties.tsx
+++ src/pages/all-properties.tsx
@@ -5,12 +5,23 @@ import { Box, Stack, Typography } from '@pankod/refine-mui';
 import { useNavigate } from '@pankod/refine-react-router-v6';
 import { PropertyCard, CustomButton } from 'components';
 
-const allProperties = () => {
+const AllProperties = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      all-properties
-    </div>
+    <Box>
+      <Stack direction="row" justifyContent="space-between" alignItems="center">
+        <Typography fontSize={25} fontWeight={700} color="#11142d">All Properties</Typography>
+        <CustomButton 
+          title="Add Property"
+          handleClick={() => {}}
+          backgroundColor="#475be8"
+          color="#fcfcfc"
+          icon={<Add />}
+          />
+      </Stack>
+    </Box>
   )
 }
 
-export default allProperties;
+export default AllProperties;
