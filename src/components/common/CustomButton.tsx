81062c2a137f2677cda728967549c1e05968050a
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-22 12:43:46 -0500

    add imports to CustomButton page and define props in component

--- src/components/common/CustomButton.tsx
+++ src/components/common/CustomButton.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Button } from '@pankod/refine-mui';
+import { CustomButtonProps } from 'interfaces/common';
 
-const CustomButton = () => {
+const CustomButton = ({ type, title, backgroundColor, color, fullWidth, icon, handleClick }: CustomButtonProps) => {
   return (
     <div>
       Custom Button
