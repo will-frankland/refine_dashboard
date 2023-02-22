5740c616ce13f00aa9e3cac82dba536f643a1e7b
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-22 15:21:38 -0500

    add styling to formcontrol

--- src/components/common/Form.tsx
+++ src/components/common/Form.tsx
@@ -16,7 +16,29 @@ import { FormProps } from "interfaces/common";
 import CustomButton from './CustomButton';
 
 const Form = ({ type, register, handleSubmit, handleImageChange, formLoading, onFinishHandler, propertyImage }: FormProps) => {
-  return <div>Form</div>;
+  return 
+  <Box>
+    <Typography fontSize={25} fontWeight={700} color="#11142d">
+      {type} a Property
+    </Typography>
+    <Box mt={2.5} borderRadius="15px" padding="20px" bgcolor="#fcfcfc">
+      <form style={{ marginTop: '20px', width: '100%', display: 'flex', flexDirection: 'column', gap: '20px'}}
+      onSubmit={handleSubmit(onFinishHandler)}
+      >
+        <FormControl>
+          <FormHelperText sx={{ fontWeight: 500, margin: '10px 0', fontSize: 16, color: "#11142d"}}>Enter proeprty name</FormHelperText>
+          <TextField 
+            fullWidth
+            required
+            id="outline-basic"
+            color="info"
+            variant="outlined"
+            {...register('title', { required: true })}
+          />
+        </FormControl>
+      </form>
+    </Box>
+  </Box>
 };
 
 export default Form;
