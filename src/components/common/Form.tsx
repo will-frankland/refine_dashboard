4d7854cd156a5463fb17f52e5046d2de57cadbad
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-23 13:04:21 -0500

    add button and input for photo file upload

--- src/components/common/Form.tsx
+++ src/components/common/Form.tsx
@@ -168,6 +168,18 @@ const Form = ({
             justifyContent='center' mb={2}>
             <Stack direction="row" gap={2}>
               <Typography color="#11142D" fontSize={16} fontWeight={500} my="10px">Property Photo</Typography>
+              <Button component="label" sx={{ width: 'fit-content', color: "#2ed480", textTransform: 'capitalize', fontSize: 16 }}>
+                Upload *
+                <input
+                  hidden
+                  accept="image/*"
+                  type="file"
+                  onChange={(e) => {
+                    // @ts-ignore
+                    handleImageChange(e.target.files[0])
+                  }}
+                />
+              </Button>
             </Stack>
           </Stack>
         </form>
